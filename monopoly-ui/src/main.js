a7849f4536595044f90a5500d80c3e0d8f2de7e3
xiaoke_1256 <xiaoke_1256@sina.com>
2026-09-13 16:44:24 +0800

    css 独立出来

--- monopoly-ui/src/main.js
+++ monopoly-ui/src/main.js
@@ -3,6 +3,7 @@ import ViewUIPlus from 'view-ui-plus'
 import 'view-ui-plus/dist/styles/viewuiplus.css'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/global.scss'
 
 const app = createApp(App)
 app.use(router)
